d1df99e5b72c9c637daf5dd38e14b09058c6999f
Carl John B. Villavito <carljohnvillavito2023@gmail.com>
2024-10-17 16:21:15 +0800

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -3,20 +3,24 @@ const path = require('path');
 
 module.exports = {
   name: 'help',
-  description: 'Show available commands',
+  description: 'Show available commands with enhanced formatting',
   author: 'System',
   execute(senderId, args, pageAccessToken, sendMessage) {
     const commandsDir = path.join(__dirname, '../commands');
     const commandFiles = fs.readdirSync(commandsDir).filter(file => file.endsWith('.js'));
 
+    // Customize the appearance of the help message
     const commands = commandFiles.map(file => {
       const command = require(path.join(commandsDir, file));
-      return `⟿ ${command.name}\n  - ${command.description}\n  - Credits: ${command.author}`;
+      return `🔹 *${command.name.toUpperCase()}*\n   - _${command.description}_\n   👤 *Author:* ${command.author}`;
     });
 
     const totalCommands = commandFiles.length;
-    const helpMessage = `Here are the available commands: \nTotal commands: ${totalCommands} \n\n${commands.join('\n\n')}`;
+    const header = `✨ Welcome to *CarlJohn Bot*! ✨\nHere are the available commands you can use:\n\n`;
+    const footer = `\n📚 _Use "${prefix}help" to rediscover this list anytime!_`;
+    const helpMessage = `${header}${commands.join('\n\n')}\n\n⚙️ *Total Commands Available:* ${totalCommands} ${footer}`;
     
+    // Send the formatted help message
     sendMessage(senderId, { text: helpMessage }, pageAccessToken);
   }
 };
